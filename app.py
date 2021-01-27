c3d280223342d6464a0fb91f4d4afadea41f4ef3
Hashfi Arissa <77279435+hasharissa@users.noreply.github.com>
2021-01-27 21:02:33 +0700

    Adding app,model and procfile

---
+++ app.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import pickle
+from flask import Flask, request, jsonify, render_template
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=["GET",'POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    
+    age_submit = request.form.get("Age")     
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+    if output == 0:
+        return render_template('index.html', prediction_text='Result: You are probably not Diabetes', age_text='Your Age {}'.format(age_submit))
+    else:
+        return render_template('index.html', prediction_text='Result: You are probably Diabetes', age_text='Your Age {}'.format(age_submit))
+
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    '''
+    For direct API calls trought request
+    '''
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+if __name__ == "__main__":
+    app.run(debug=True)
